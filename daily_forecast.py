3cb332f3931798caa03bf9825c9783e922c46f00
ereztzafar <erez.zafar@gmail.com>
2025-07-31 09:41:44 +0300

    Update daily_forecast.py

--- daily_forecast.py
+++ daily_forecast.py
@@ -1,7 +1,7 @@
 from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
-from flatlib.aspects import getAngle, aspectType, MAJOR_ASPECTS
+from flatlib.aspects import AspectChecker, MAJOR_ASPECTS
 from flatlib import const
 from lotto_bot import send_telegram_message
 from datetime import datetime
@@ -12,7 +12,6 @@ BIRTH_TIME = '06:00'
 TIMEZONE = '+02:00'
 BIRTH_PLACE = GeoPos('32n05', '34e53')
 
-# שימוש בקבועים מתוך const
 OBJECTS = [
     const.SUN, const.MOON, const.MERCURY, const.VENUS, const.MARS,
     const.JUPITER, const.SATURN, const.URANUS, const.NEPTUNE, const.PLUTO,
@@ -24,20 +23,21 @@ def start_forecast():
     header = f"📆 תחזית ליום {now.strftime('%d/%m/%Y')}:\n"
     header += "🔭 מפת לידה והיבטים בין כוכבים:\n"
 
-    # יצירת מפת לידה
     birth_dt = Datetime(BIRTH_DATE, BIRTH_TIME, TIMEZONE)
     chart = Chart(birth_dt, BIRTH_PLACE, IDs=OBJECTS)
 
+    aspect_checker = AspectChecker(chart, aspects=MAJOR_ASPECTS)
+    found_aspects = aspect_checker.allAspects()
+
     aspect_lines = []
-    for i in range(len(OBJECTS)):
-        for j in range(i + 1, len(OBJECTS)):
-            obj1 = chart.get(OBJECTS[i])
-            obj2 = chart.get(OBJECTS[j])
-            angle = getAngle(obj1, obj2)
-            a_type = aspectType(obj1, obj2, MAJOR_ASPECTS)
-            if a_type:
-                line = f"🔹 {obj1.id} ({obj1.sign}) ⬄ {obj2.id} ({obj2.sign}) - היבט: {a_type} ({round(angle)}°)"
-                aspect_lines.append(line)
+    for aspect in found_aspects:
+        obj1 = aspect.obj1
+        obj2 = aspect.obj2
+        aspect_type = aspect.type
+        angle = round(aspect.orb)  # Or use aspect.angle if needed
+
+        line = f"🔹 {obj1} ({chart.get(obj1).sign}) ⬄ {obj2} ({chart.get(obj2).sign}) - היבט: {aspect_type} ({angle}°)"
+        aspect_lines.append(line)
 
     message = header + "\n".join(aspect_lines)
     print(message)
