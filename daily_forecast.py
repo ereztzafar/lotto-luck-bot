810e06d101997e25cca59ee5b85ac5241399048e
ereztzafar <erez.zafar@gmail.com>
2025-08-01 10:01:04 +0300

    Update daily_forecast.py

--- daily_forecast.py
+++ daily_forecast.py
@@ -1,4 +1,4 @@
-from flatlib import const
+from flatlib import const, aspects
 from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
@@ -7,33 +7,31 @@ import datetime
 # מיקום הלידה הקבוע שלך (פתח תקווה)
 BIRTH_LOCATION = GeoPos("32n5", "34e53")  # פתח תקווה לפי פורמט flatlib
 
-def find_lucky_hours(birth_chart, current_chart):
+def find_lucky_hours(birth_chart):
     """
-    משווה בין מפת הלידה למפת הטרנזיט ומחזירה שעות מזל להגרלות (לוטו, חישגד, צ'אנס).
-    נבדק היבט חיובי של הירח מול שמש/ונוס/צדק – אם אין נסיגה שפוגעת באיכות.
+    מחזירה שעות מזל לפי היבטים חיוביים בין הירח במפת הטרנזיט לבין שמש/ונוס/צדק במפת הלידה.
     """
     lucky_hours = []
     now = datetime.datetime.now()
 
-    # בדיקת כל שעתיים היום (מ־05:00 עד 23:00)
     for hour in range(5, 24, 2):
         forecast_time = now.replace(hour=hour, minute=0, second=0, microsecond=0)
-        date_str = forecast_time.strftime('%Y/%m/%d')  # פורמט נכון ל־Flatlib
+        date_str = forecast_time.strftime('%Y/%m/%d')
         time_str = forecast_time.strftime('%H:%M')
 
         transit_chart = Chart(Datetime(date_str, time_str, '+02:00'), BIRTH_LOCATION)
         moon = transit_chart.get(const.MOON)
 
-        # בדיקת היבטים חיוביים מול שמש, ונוס, צדק
         targets = [const.SUN, const.VENUS, const.JUPITER]
         for target in targets:
             natal_obj = birth_chart.get(target)
-            aspects = moon.aspectList([natal_obj])
 
-            for asp in aspects:
+            # קבלת רשימת ההיבטים לירח מול הכוכב
+            aspects_list = aspects.getAspects(moon, [natal_obj])
+            for asp in aspects_list:
                 if asp.type in [const.TRINE, const.SEXTILE]:
                     retro_note = ' (🔁 נסיגה)' if natal_obj.retro else ''
                     lucky_hours.append(f"{time_str} – {asp.type} ל־{target}{retro_note}")
-                    break  # מספיק היבט חיובי אחד
+                    break
 
     return lucky_hours
