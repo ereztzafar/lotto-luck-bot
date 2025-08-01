dd2cf855e3598ecb8f72b5e65abce277e60de537
ereztzafar <erez.zafar@gmail.com>
2025-08-01 08:44:50 +0300

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,6 +1,14 @@
 import requests
 import json
 
+def load_secrets():
+    token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    if not token or not chat_id:
+        raise Exception("❌ חסר TELEGRAM_TOKEN או TELEGRAM_CHAT_ID ב־Secrets")
+    return token, chat_id
+
+
 def send_telegram_message(message, token, chat_id):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {
