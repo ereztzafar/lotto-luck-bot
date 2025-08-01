eaec86231f22588a3da9764b9bfd14b2dce274e6
ereztzafar <erez.zafar@gmail.com>
2025-08-01 08:11:52 +0300

    Update daily_forecast.py

--- daily_forecast.py
+++ daily_forecast.py
@@ -1,3 +1,13 @@
-# daily_forecast.py placeholder
+from flatlib import const
+from flatlib.chart import Chart
+from flatlib.datetime import Datetime
+from flatlib.geopos import GeoPos
 
-# TODO: Replace with actual code
+def generate_forecast(date, time, location_str):
+    pos = GeoPos(*location_str.split(','))
+    chart = Chart(Datetime(date, time, '+02:00'), pos)
+    aspects = chart.getAspects()
+    forecast = []
+    for asp in aspects:
+        forecast.append(f"{asp.obj1} {asp.type} {asp.obj2}")
+    return forecast
