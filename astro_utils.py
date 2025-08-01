7b0a483b2aa60666b53231e8c117f05e56fd1ba2
ereztzafar <erez.zafar@gmail.com>
2025-08-01 08:10:29 +0300

    Update astro_utils.py

--- astro_utils.py
+++ astro_utils.py
@@ -1,3 +1,12 @@
-# astro_utils.py placeholder
+from flatlib.geopos import GeoPos
+from flatlib.chart import Chart
+from flatlib.datetime import Datetime
 
-# TODO: Replace with actual code
+def create_chart(date_str, time_str, location_str):
+    """
+    Create an astrology chart from date, time, and location.
+    """
+    date = date_str
+    time = time_str
+    pos = GeoPos(*location_str.split(','))
+    return Chart(Datetime(date, time, '+02:00'), pos)
