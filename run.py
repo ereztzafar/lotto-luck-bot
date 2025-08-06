4e6a075e4978b0093287f711f0a0e184196808bb
ereztzafar <erez.zafar@gmail.com>
2025-08-06 10:30:45 +0300

    Update run.py

--- run.py
+++ run.py
@@ -33,7 +33,7 @@ PLANETS = [
 ALL_OBJECTS = PLANETS + ['FORTUNE']
 
 # === כוכבים שקשורים לכסף ===
-MONEY_OBJECTS = [const.VENUS, const.JUPITER, const.MERCURY, const.MOON, const.PLUTO, 'FORTUNE']
+MONEY_OBJECTS = [const.VENUS, const.JUPITER, const.MOON, const.PLUTO, 'FORTUNE']
 
 # === זוויות חשובות ===
 HARMONIC_ANGLES = [0, 60, 120, 180]
